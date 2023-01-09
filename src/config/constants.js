4688d3f41f51dd360876b7c7ad67c6b8356d205e
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-01-09 15:20:29 +0100

    changed the url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://the-mensroom-barbershop.onrender.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
