ac1293a91097200209c4c568c41cbc7ca3be7529
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-02-21 11:22:33 +0100

    changed the map size

--- src/components/Map.js
+++ src/components/Map.js
@@ -51,41 +51,41 @@ function Map() {
           </a>
         </div>
       </div>
+      <div>
+        <MapContainer
+          style={{
+            borderRadius: "10px",
+            height: "30vw",
+            width: "90%",
+            margin: "auto",
+            backgroundColor: "black",
+          }}
+          center={[52.355767, 4.900297]}
+          zoom={13}
+        >
+          <TileLayer
+            style={{ color: "white" }}
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-      <MapContainer
-        style={{
-          borderRadius: "10px",
-          height: "30vw",
-          width: "60vw",
-          margin: "auto",
-          backgroundColor: "black",
-        }}
-        center={[52.355767, 4.900297]}
-        zoom={13}
-      >
-        <TileLayer
-          style={{ color: "white" }}
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <Marker position={[52.355767, 4.900297]}>
-          <Popup>
-            <h3>
-              THE MENSROOM
-              <br />
-              barbershop
-            </h3>
-            <a
-              href="https://www.google.com/maps/dir/52.3998817,4.9263348/The+men%E2%80%99s+Room,+Utrechtsestraat,+Amsterdam/@52.3799452,4.8731692,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x47c609241acb6ad5:0xc94b4d46d6214419!2m2!1d4.8986634!2d52.3628352"
-              style={{ fontSize: "20px" }}
-            >
-              Direction
-            </a>
-          </Popup>
-        </Marker>
-      </MapContainer>
-
+          <Marker position={[52.355767, 4.900297]}>
+            <Popup>
+              <h3>
+                THE MENSROOM
+                <br />
+                barbershop
+              </h3>
+              <a
+                href="https://www.google.com/maps/dir/52.3998817,4.9263348/The+men%E2%80%99s+Room,+Utrechtsestraat,+Amsterdam/@52.3799452,4.8731692,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x47c609241acb6ad5:0xc94b4d46d6214419!2m2!1d4.8986634!2d52.3628352"
+                style={{ fontSize: "20px" }}
+              >
+                Direction
+              </a>
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
       <div className="last">
         <div>
           © 2022 Mensroom Amsterdam. All Rights Reserved. Built by Mahtab.
