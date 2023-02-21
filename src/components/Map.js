f48c8dcac91c7d55979d84bedbab69eeff8d2297
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-02-21 11:35:30 +0100

    changed the location address size

--- src/components/Map.js
+++ src/components/Map.js
@@ -25,17 +25,23 @@ function Map() {
         </div>
         <div className="flex2">
           <a
+            className="emailnumber"
             style={{ color: "white" }}
             href="mailto:mahtabesmaeilii13@gmail.com"
           >
             mahtabesmaeilii13@gmail.com
-          </a>{" "}
+          </a>
           <br />
-          <a style={{ color: "white" }} href="tel:+31648391204">
+          <a
+            className="emailnumber"
+            style={{ color: "white" }}
+            href="tel:+31648391204"
+          >
             +31648391204
           </a>
           <a href="https://api.whatsapp.com/send?phone=31648391204">
             <img
+              className="emailnumber"
               style={{
                 width: "25px",
                 display: "block",
